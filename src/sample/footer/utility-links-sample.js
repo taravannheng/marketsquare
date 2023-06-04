775fb0c1780cd66d2b60b4a2409c250e1113aef4
Taravann Heng <heng.t.taravann@gmail.com>
2023-06-04 19:08:04 +1000

    feat(data): add id to utility link sample data

--- src/sample/footer/utility-links-sample.js
+++ src/sample/footer/utility-links-sample.js
@@ -1,36 +1,40 @@
 const footerUtilityLinksSample = [
   {
+    utilityId: 'util-R3hT9jXm5vE8',
     title: "Marketsquare",
     links: [
-      { text: "About Us", route: '/' },
-      { text: "Career", route: '/' },
-      { text: "Privacy Policy", route: '/' },
-      { text: "Terms of Service", route: '/' },
+      { linkId: 'util-J5eD8hTm1wR9', text: "About Us", route: '/' },
+      { linkId: 'util-S7kG3pXq6vN4', text: "Career", route: '/' },
+      { linkId: 'util-C2mE9jZp4xW7', text: "Privacy Policy", route: '/' },
+      { linkId: 'util-H9nT6gRc3yF2', text: "Terms of Service", route: '/' },
     ],
   },
   {
+    utilityId: 'util-G6sN7yPc2dA1',
     title: "Products",
     links: [
-      { text: "Track Your Order", route: "/" },
-      { text: "Shipping & Returns", route: "/" },
-      { text: "Explore Our Products", route: "/" },
+      { linkId: 'util-L4wN3fTj2dS8', text: "Track Your Order", route: "/" },
+      { linkId: 'util-Q1vM5sPd9zC3', text: "Shipping & Returns", route: "/" },
+      { linkId: 'util-A8rG4tYk6wE9', text: "Explore Our Products", route: "/" },
     ],
   },
   {
+    utilityId: 'util-F9kM2qRj7wE6',
     title: "Community",
     links: [
-      { text: "Join Our Community", route: "/" },
-      { text: "Follow Us", route: "/" },
-      { text: "Subscribe to Our Newsletter", route: "/" },
-      { text: "Connect with Us", route: "/" },
+      { linkId: 'util-U3cX2mHv5dP1', text: "Join Our Community", route: "/" },
+      { linkId: 'util-Z6bT9wSv7kM2', text: "Follow Us", route: "/" },
+      { linkId: 'util-O9fL2rNc1yG3', text: "Subscribe to Our Newsletter", route: "/" },
+      { linkId: 'util-E4jQ7kWd5pH2', text: "Connect with Us", route: "/" },
     ],
   },
   {
+    utilityId: 'util-B4pT8sGk1cN3',
     title: "Support",
     links: [
-      { text: "Contact us", route: "/" },
-      { text: "FAQs", route: "/" },
-      { text: "Feedback", route: "/" },
+      { linkId: 'util-I7vM1cDz6bR9', text: "Contact us", route: "/" },
+      { linkId: 'util-Y2gF5xPc8nT3', text: "FAQs", route: "/" },
+      { linkId: 'util-V3hS9mRk1gX4', text: "Feedback", route: "/" },
     ],
   },
 ];
