1ee28ace6f82d2177fa33e6e4df05f354e4df8b3
Taravann Heng <heng.t.taravann@gmail.com>
2023-06-18 03:17:33 +1000

    feat(component): add search component
    close #21

---
+++ src/components/searchbox/searchbox.component.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import { useMediaQuery } from '@mui/material';
+
+import SearchBoxInterface from './searchbox.interface';
+import { IconSC, SearchBoxSC, TextFieldSC } from './searchbox.style';
+
+
+const SearchBox: FC<SearchBoxInterface> = ({ searchTerm, setSearchTerm }) => {
+  const isSmallScreen = useMediaQuery("(max-width: 639px)");
+
+  const handleSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+    console.log(event.target.value)
+  }
+
+  return (
+      <SearchBoxSC>
+        <IconSC>
+          <SearchIcon />
+        </IconSC>
+        <TextFieldSC
+          sx={{ width: '100%'}}
+          hiddenLabel
+          label=""
+          variant="filled"
+          value={searchTerm}
+          placeholder="Search products..."
+          onChange={handleSearchTermChange}
+        />
+      </SearchBoxSC>
+  )
+}
+
+export default SearchBox;
---
+++ src/components/searchbox/searchbox.interface.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+export default interface SearchBoxInterface {
+  children?: ReactNode;
+  searchTerm: string;
+  setSearchTerm: (searchTerm: string) => void;
+}
