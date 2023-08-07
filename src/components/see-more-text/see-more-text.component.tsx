7853f327d122ddebd00ba1f5e2de45117e69896b
Taravann Heng <heng.t.taravann@gmail.com>
2023-08-07 20:01:15 +1000

    feat(see-more-text): add see more text functionality

---
+++ src/components/see-more-text/see-more-text.component.tsx
@@ -0,0 +1,24 @@
+import { FC, useState } from 'react';
+
+import SeeMoreTextI from './see-more-text.interface';
+import { ButtonSC, SeeMoreTextSC } from './see-more-text.styles';
+
+const SeeMoreText: FC<SeeMoreTextI> = ({ children, defaultTextLength = 100 }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const shortenedText = children!.slice(0, defaultTextLength) + '...';
+
+  const clickHandler = () => {
+    setIsExpanded(!isExpanded);
+  }
+
+  return (
+    <SeeMoreTextSC>
+      {isExpanded ? children : shortenedText}
+      <ButtonSC onClick={clickHandler} disableRipple>
+        {isExpanded ? 'See Less' : 'See More'}
+      </ButtonSC>
+    </SeeMoreTextSC>
+  )
+}
+
+export default SeeMoreText;
