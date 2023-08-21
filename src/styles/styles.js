66c574b32de1d7c6c2ed5791bfa737cae7a926b6
Taravann Heng <heng.t.taravann@gmail.com>
2023-08-21 21:05:38 +1000

    feat(styles): add style object for easier import

---
+++ src/styles/styles.js
@@ -0,0 +1,15 @@
+import space from "./spacing";
+import colors from "./colors";
+import typography from "./typography";
+import borderRadius from "./border-radius";
+import shadows from "./shadows";
+
+const styles = {
+  space,
+  colors,
+  typography,
+  borderRadius,
+  shadows
+};
+
+export default styles;
