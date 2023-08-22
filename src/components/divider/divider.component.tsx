fcf9d8be2a9c76881fc1508eb38f7e2fb9a4f48e
Taravann Heng <heng.t.taravann@gmail.com>
2023-08-23 00:23:15 +1000

    refactor(divider): change file names

--- src/components/divider/divider.component.tsx
+++ src/components/divider/divider.component.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 
-import DividerInterface from './index.interface';
-import { DividerSC } from './index.styles';
+import DividerInterface from './divider.interface';
+import { DividerSC } from './divider.styles';
 
 const Divider: FC<DividerInterface> = ({ width, height, color }) => {
   return (
