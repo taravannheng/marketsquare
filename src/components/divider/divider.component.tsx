80bd5773f61bb240d2110c93e65441670ab85b58
Taravann Heng <heng.t.taravann@gmail.com>
2023-08-23 00:30:11 +1000

    refactor(divider): add default values

--- src/components/divider/divider.component.tsx
+++ src/components/divider/divider.component.tsx
@@ -1,12 +1,19 @@
-import { FC } from 'react'
+import { FC } from "react";
 
-import DividerInterface from './divider.interface';
-import { DividerSC } from './divider.styles';
+import DividerInterface from "./divider.interface";
+import { DividerSC } from "./divider.styles";
+import { colors } from '../../styles/styles';
 
 const Divider: FC<DividerInterface> = ({ width, height, color }) => {
   return (
-    <DividerSC sx={{ width: `${width && width} !important`, height: `${height && height} !important`, backgroundColor: `${color && color} !important` }} />
-  )
-}
+    <DividerSC
+      sx={{
+        width: `${width ?? '100%'} !important`,
+        height: `${height ?? '1px'} !important`,
+        backgroundColor: `${color ?? colors.light} !important`,
+      }}
+    />
+  );
+};
 
 export default Divider;
