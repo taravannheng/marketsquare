bed71a951c893cc647796e5cac89d39fbe914923
Taravann Heng <heng.t.taravann@gmail.com>
2023-08-29 17:50:22 +1000

    refactor(colors): add more colors to color palette

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,13 +1,53 @@
-const colors = {
-  lightPrimary: '#758AE3',
-  primary: '#536EDD',
-  lightest: '#FFF',
-  light: '#F2F2F2',
-  grey: '#888B94',
-  dark: '#575C70',
-  darkest: '#12162A',
-  red: '#FD3F62',
-  green: '#14D18B',
+const COLORS = {
+  PRIMARY: {
+    P50: '#F1F4FD',
+    P100: '#E0E6F9',
+    P200: '#C7D3F6',
+    P300: '#A1B8EF',
+    P400: '#7492e6',
+    P500: '#536EDD', // primary
+    P600: '#3F52D1',
+    P700: '#3641BF',
+    P800: '#31369C',
+    P900: '#2C327C',
+  },
+  NEUTRAL: {
+    N0: '#FFFFFF',
+    N50: '#F6F6F9',
+    N100: '#EDEDF1',
+    N200: '#D7D8E0',
+    N300: '#B4B6C5',
+    N400: '#8B8DA5',
+    N500: '#6D708A', // neutral
+    N600: '#575972',
+    N700: '#47485D',
+    N800: '#3E3F4E',
+    N900: '#363744',
+  },
+  GREEN: {
+    G50: '#ECFDF4',
+    G100: '#D1FAE3',
+    G200: '#A8F2CD',
+    G300: '#6FE6B2',
+    G400: '#36D191',
+    G500: '#10A36C', // green
+    G600: '#079462',
+    G700: '#057751',
+    G800: '#075E42',
+    G900: '#074D38',
+  },
+  RED: {
+    R50: '#FFF0F1',
+    R100: '#FFE3E5',
+    R200: '#FFCBD2',
+    R300: '#FFA0AD',
+    R400: '#FF6A82',
+    R500: '#FD3056', // red
+    R600: '#EB1345',
+    R700: '#C7093B',
+    R800: '#A60B38',
+    R900: '#8E0D37',
+  },
 }
 
-export default colors;
+export default COLORS;
