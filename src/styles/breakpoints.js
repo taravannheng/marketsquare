7fcbc8231bedd7c510c6cdce2f2922911c1ebff7
Taravann Heng <heng.t.taravann@gmail.com>
2023-08-29 17:22:46 +1000

    feat(styles): add breakpoints

---
+++ src/styles/breakpoints.js
@@ -0,0 +1,10 @@
+const BREAKPOINTS = {
+  xs: 390,
+  sm: 768,
+  md: 992,
+  lg: 1200,
+  xl: 1280,
+  xxl: 1536,
+}
+
+export default BREAKPOINTS;
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3,5 +3,6 @@ import colors from "./colors";
 import typography from "./typography";
 import borderRadius from "./border-radius";
 import shadows from "./shadows";
+import BREAKPOINTS from "./breakpoints";
 
-export { space, colors, typography, borderRadius, shadows };
+export { space, colors, typography, borderRadius, shadows, BREAKPOINTS };
