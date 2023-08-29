23d4413366319cb19ae22f0e46b0b7d1a7a82ea1
Taravann Heng <heng.t.taravann@gmail.com>
2023-08-29 17:59:22 +1000

    refactor(styles): replace old color import with new color import

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,7 +1,7 @@
 import space from "./spacing";
-import colors from "./colors";
+import COLORS from "./colors";
 import typography from "./typography";
 import borderRadius from "./border-radius";
 import shadows from "./shadows";
 
-export { space, colors, typography, borderRadius, shadows };
+export { space, COLORS, typography, borderRadius, shadows };
