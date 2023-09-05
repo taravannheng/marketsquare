948b96c09a4037bd260548c33a25da039cbb370e
Taravann Heng <heng.t.taravann@gmail.com>
2023-09-06 01:58:12 +0700

    refactor(button): revamp button component

---
+++ src/components/button/button.interface.ts
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+export default interface ButtonProps {
+  children: ReactNode;
+  actionType?: "button" | "submit";
+  styleType?: "primary" | "secondary" | "tertiary";
+  isLoading?: boolean;
+  disabled?: boolean;
+  icon?: ReactNode;
+  iconPosition?: "left" | "right";
+  width?: "full" | "auto";
+  clickHandler?: (() => void) | ((event: any) => Promise<void>);
+  href?: string;
+  underlineLabel?: boolean;
+}
