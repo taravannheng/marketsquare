72c649cffd4afdc8c591064e64070c7cb75a895c
Taravann Heng <heng.t.taravann@gmail.com>
2023-09-12 13:25:39 +0700

    feat(review): add review api

---
+++ src/apis/reviews/reviews.api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: `${process.env.REACT_APP_BASE_URL}`,
+});
+
+export const createReview = (review) => api.post(`/api/reviews`, { review });
+export const getReview = (reviewID) => api.get(`/api/reviews/${reviewID}`);
+export const getMultipleReviews = (reviewIDs) =>
+  api.get(`/api/reviews/batch`, { params: { ids: reviewIDs.join(",") } });
+export const getReviewsByProductID = (productID) => api.get(`/api/reviews/product/${productID}`);
+export const getReviews = () => api.get(`/api/reviews`);
+export const updateReview = (review, reviewID) => api.patch(`/api/reviews/${reviewID}`, { review });
+export const deleteReview = (reviewID) => api.delete(`/api/reviews/${reviewID}`);
