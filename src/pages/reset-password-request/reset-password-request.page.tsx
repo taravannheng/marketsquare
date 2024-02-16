11ea395b4c8e7f949ca84ceed9dee978789d717c
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-16 20:36:52 +0700

    refactor(components): remove footer from sign up, sign in and reset password page
    close #289

--- src/pages/reset-password-request/reset-password-request.page.tsx
+++ src/pages/reset-password-request/reset-password-request.page.tsx
@@ -2,8 +2,6 @@ import { FC } from 'react';
 
 import ResetPasswordRequestForm from '../../components/reset-password-request-form/reset-password-request-form.component';
 import Header from '../../components/header/index.component';
-import Footer from '../../components/footer/index.component';
-import footerItemsSample from "../../sample/footer/utility-links-sample";
 import { FormContainerSC, PageSC } from './reset-password-request.styles';
 
 const ResetPasswordRequestPage: FC = () => {
@@ -14,7 +12,6 @@ const ResetPasswordRequestPage: FC = () => {
       <FormContainerSC>
         <ResetPasswordRequestForm />
       </FormContainerSC>
-      <Footer footerItems={footerItemsSample} />
     </PageSC>
     </>
   )
