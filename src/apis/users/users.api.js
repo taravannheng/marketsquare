5b939495c9c8df2f3bf7f6bb9b3683934ae2a42c
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-16 17:25:22 +0700

    feat(users-api): add functions for updating username and email

--- src/apis/users/users.api.js
+++ src/apis/users/users.api.js
@@ -11,4 +11,6 @@ export const getUser = (token) => api.get(`/api/users`, {
     Authorization: `Bearer ${token}` 
   } 
 });
+export const updateUsername = (email, username) => api.patch(`/api/users/${email}/username`, { username });
+export const updateEmail = (email, newEmail) => api.patch(`/api/users/${email}/email`, { newEmail });
 export const updatePassword = (email, password) => api.patch(`/api/users/${email}/password`, { password });
