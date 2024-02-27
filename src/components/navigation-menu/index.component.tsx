13f9b9ed41544b8e2a0a8045fcfe152740289518
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-27 21:35:22 +0700

    refactor(navigation-menu): change interface name to props

--- src/components/navigation-menu/index.component.tsx
+++ src/components/navigation-menu/index.component.tsx
@@ -3,11 +3,11 @@ import _ from 'lodash';
 import { v4 as uuidv4 } from 'uuid';
 
 import NavigationMenuItem from '../navigation-menu-item/index.component';
-import NavigationMenuInterface from './index.interface'
+import NavigationMenuProps from './index.interface'
 import { MenuInterface } from './index.interface';
 import { NavigationMenuSC, NavigationMenuStackSC } from './index.styles';
 
-const NavigationMenu: FC<NavigationMenuInterface> = ({ menuList }) => {
+const NavigationMenu: FC<NavigationMenuProps> = ({ menuList }) => {
   return (
     <NavigationMenuSC>
       <NavigationMenuStackSC direction="row"> 
