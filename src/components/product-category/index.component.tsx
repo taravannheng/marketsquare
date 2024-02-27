307ee8d409c8e7cb31db3e742d356248d94b7201
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-27 21:43:04 +0700

    refactor(product-category): change interface name to props

--- src/components/product-category/index.component.tsx
+++ src/components/product-category/index.component.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react'
 
-import ProductCategoryInterface from './index.interface';
+import ProductCategoryProps from './index.interface';
 import { LeftContainerContentSC, LeftContainerSC, ProductCategorySC, ProductContainerSC, RightContainerBottomContentSC, RightContainerSC, RightContainerTopContentSC, TitleSC } from './index.styles';
 
-const ProductCategory: FC<ProductCategoryInterface> = ({ title, images }) => {
+const ProductCategory: FC<ProductCategoryProps> = ({ title, images }) => {
   return (
     <ProductCategorySC>
       <TitleSC variant='h5'>{ title }</TitleSC>
