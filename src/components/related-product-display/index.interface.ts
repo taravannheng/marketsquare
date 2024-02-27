29a32ab6efab37665fb3148da30b99944972d5d5
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-27 21:47:20 +0700

    refactor(related-product-display): change interface name to props

--- src/components/related-product-display/index.interface.ts
+++ src/components/related-product-display/index.interface.ts
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 import ProductInterface from "../../interfaces/product-interface";
 
-export default interface RelatedProductDisplayInterface {
+export default interface RelatedProductDisplayProps {
   children?: ReactNode;
   products: ProductInterface[] | null;
 }
