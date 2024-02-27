3b68d8fada9bb2bdd50967da9d3d89b7956538fb
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-27 21:33:49 +0700

    refactor(loading-screen): change interface name to props

--- src/components/loading-screen/index.component.tsx
+++ src/components/loading-screen/index.component.tsx
@@ -1,10 +1,10 @@
 import { FC } from 'react';
 
-import LoadingScreenInterface from './index.interface';
+import LoadingScreenProps from './index.interface';
 import { LoadingScreenSC } from './index.styles';
 import { LOGO_URLS } from '../../utils/constants';
 
-const LoadingScreen: FC<LoadingScreenInterface> = () => {
+const LoadingScreen: FC<LoadingScreenProps> = () => {
   return (
     <LoadingScreenSC>
       <img alt='logo animation' src={LOGO_URLS.ANIMATION} width="120" height="120" />
