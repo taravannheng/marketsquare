37b54e967c4b0aab7a2c6bb7d7bfa94a491d7c6e
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-27 21:36:17 +0700

    refactor(navigation-menu-item): change interface name to props

--- src/components/navigation-menu-item/index.component.tsx
+++ src/components/navigation-menu-item/index.component.tsx
@@ -1,44 +1,80 @@
-import { FC, useState } from 'react';
-import _ from 'lodash';
-import { v4 as uuidv4 } from 'uuid';
+import { FC, useState } from "react";
+import _ from "lodash";
+import { v4 as uuidv4 } from "uuid";
 
-import NavigationMenuItemInterface from './index.interface';
-import { CategoryContainerSC, CategoryItemSC, CategoryItemStackSC, CategoryItemTitleSC, CategorySC, LinkSC, MenuSC, MenuTextSC, NavigationMenuItemSC, SubMenuMediaSC, SubMenuSC } from './index.styles';
+import NavigationMenuItemProps from "./index.interface";
+import {
+  CategoryContainerSC,
+  CategoryItemSC,
+  CategoryItemStackSC,
+  CategoryItemTitleSC,
+  CategorySC,
+  LinkSC,
+  MenuSC,
+  MenuTextSC,
+  NavigationMenuItemSC,
+  SubMenuMediaSC,
+  SubMenuSC,
+} from "./index.styles";
 
-const NavigationMenuItem: FC<NavigationMenuItemInterface> = ({ imgUrl, categories, title }) => {
+const NavigationMenuItem: FC<NavigationMenuItemProps> = ({
+  imgUrl,
+  categories,
+  title,
+}) => {
   const [showSubMenu, setShowSubMenu] = useState(false);
-  
+
   const showDropdownHandler = () => {
     setShowSubMenu(true);
-  }
+  };
 
   const hideDropdownHandler = () => {
     setShowSubMenu(false);
-  }
+  };
 
   return (
     <NavigationMenuItemSC>
-      <MenuSC sx={{ backgroundColor: `${showSubMenu && '#758AE3'}` }} onClick={showDropdownHandler} onMouseLeave={hideDropdownHandler} onMouseEnter={showDropdownHandler}><MenuTextSC sx={{ color: `${showSubMenu && '#FFF'}` }}>{title}</MenuTextSC></MenuSC>
-      {showSubMenu && <SubMenuSC onMouseEnter={showDropdownHandler} onMouseLeave={hideDropdownHandler}>
+      <MenuSC
+        sx={{ backgroundColor: `${showSubMenu && "#758AE3"}` }}
+        onClick={showDropdownHandler}
+        onMouseLeave={hideDropdownHandler}
+        onMouseEnter={showDropdownHandler}
+      >
+        <MenuTextSC sx={{ color: `${showSubMenu && "#FFF"}` }}>
+          {title}
+        </MenuTextSC>
+      </MenuSC>
+      {showSubMenu && (
+        <SubMenuSC
+          onMouseEnter={showDropdownHandler}
+          onMouseLeave={hideDropdownHandler}
+        >
           <CategoryContainerSC direction="row" spacing={8}>
-          {!_.isEmpty(categories) && categories.map((category: any) => {
-            return <CategorySC key={category.id}>
-              <CategoryItemTitleSC>{category.title}</CategoryItemTitleSC>
-              <CategoryItemStackSC direction="column" spacing={1.5}>
-              {!_.isEmpty(category.categoryListItem) && category.categoryListItem.map((categoryItem: any) => {
-                return <CategoryItemSC key={categoryItem.id}>
-                 <LinkSC href={categoryItem.url}>{categoryItem.text}</LinkSC> 
-                </CategoryItemSC>
+            {!_.isEmpty(categories) &&
+              categories.map((category: any) => {
+                return (
+                  <CategorySC key={category.id}>
+                    <CategoryItemTitleSC>{category.title}</CategoryItemTitleSC>
+                    <CategoryItemStackSC direction="column" spacing={1.5}>
+                      {!_.isEmpty(category.categoryListItem) &&
+                        category.categoryListItem.map((categoryItem: any) => {
+                          return (
+                            <CategoryItemSC key={categoryItem.id}>
+                              <LinkSC href={categoryItem.url}>
+                                {categoryItem.text}
+                              </LinkSC>
+                            </CategoryItemSC>
+                          );
+                        })}
+                    </CategoryItemStackSC>
+                  </CategorySC>
+                );
               })}
-              </CategoryItemStackSC>
-            </CategorySC>
-          })}
-
           </CategoryContainerSC>
-        </SubMenuSC>}
-      
+        </SubMenuSC>
+      )}
     </NavigationMenuItemSC>
-  )
-}
+  );
+};
 
 export default NavigationMenuItem;
