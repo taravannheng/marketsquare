ed693428b54f38d3874451806697b98206774047
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-27 21:49:52 +0700

    refactor(review-display): chagne interface name to props

--- src/components/review-display/index.interface.ts
+++ src/components/review-display/index.interface.ts
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 import ReviewInterface from "../review/index.interface";
 
-export default interface ReviewDisplayInterface {
+export default interface ReviewDisplayProps {
   children?: ReactNode;
   reviews: ReviewInterface[] | null;
   productID: string;
