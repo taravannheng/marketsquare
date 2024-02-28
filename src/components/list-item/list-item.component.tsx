ae36ce50a5dc624bcba0d9b80d7d4ab474a9da29
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-28 13:04:01 +0700

    refactor(list-item): re-arrange import statements

--- src/components/list-item/list-item.component.tsx
+++ src/components/list-item/list-item.component.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
 
+// props or interfaces imports
 import ListItemComponentInterface from "./list-item.interface";
+
+// styling imports
 import {
   ListItemSC,
   ListItemButtonSC,
