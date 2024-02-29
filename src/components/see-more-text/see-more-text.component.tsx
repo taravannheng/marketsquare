4f106db7c0cbdbdee05d82ccf06476acf44686de
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-29 16:32:17 +0700

    refactor(see-more-text): re-arrange import statements

--- src/components/see-more-text/see-more-text.component.tsx
+++ src/components/see-more-text/see-more-text.component.tsx
@@ -1,23 +1,35 @@
-import { FC, useState } from 'react';
+import { FC, useState } from "react";
 
-import SeeMoreTextI from './see-more-text.interface';
-import { ButtonSC, SeeMoreTextSC } from './see-more-text.styles';
+// props or interfaces imports
+import SeeMoreTextI from "./see-more-text.interface";
 
-const SeeMoreText: FC<SeeMoreTextI> = ({ children, defaultTextLength = 100 }) => {
+// styling imports
+import { ButtonSC, SeeMoreTextSC } from "./see-more-text.styles";
+
+const SeeMoreText: FC<SeeMoreTextI> = ({
+  children,
+  defaultTextLength = 100,
+}) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const shortenedText = children && children.slice(0, defaultTextLength) + '...';
+  const shortenedText =
+    children && children.slice(0, defaultTextLength) + "...";
 
   const clickHandler = () => {
     setIsExpanded(!isExpanded);
-  }
+  };
 
   return (
     <SeeMoreTextSC>
-      {isExpanded ? children : shortenedText}<ButtonSC onClick={clickHandler} disableRipple  sx={{ display: isExpanded ? 'block !important' : 'inline !important' }}>
-        {isExpanded ? `See Less` : 'See More'}
+      {isExpanded ? children : shortenedText}
+      <ButtonSC
+        onClick={clickHandler}
+        disableRipple
+        sx={{ display: isExpanded ? "block !important" : "inline !important" }}
+      >
+        {isExpanded ? `See Less` : "See More"}
       </ButtonSC>
     </SeeMoreTextSC>
-  )
-}
+  );
+};
 
 export default SeeMoreText;
