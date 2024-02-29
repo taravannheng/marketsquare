ea2c7a34ccd1001e54702a0faa3a8137242711b4
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-29 11:57:15 +0700

    refactor(order-list-item): re-arrange import statements

--- src/components/order-list-item/order-list-item.component.tsx
+++ src/components/order-list-item/order-list-item.component.tsx
@@ -1,63 +1,89 @@
-import { FC, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
+import { FC, useState } from "react";
 
-import OrderListItemProps, { ProductListProps, ProductItemProps } from './order-list-item.interface';
-import { DateSC, ContentSC, ListItemSC, MediaSC, IDSC, ProductItemSC, ProductNameSC, ProductPriceSC } from './order-list-item.styles';
+// 3rd-party dependencies imports
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import SnackBar from '../snackbar/snackbar.component';
-import { selectProduct } from '../../store/product/product.selector';
-import { selectUser } from '../../store/user/user.selector';
-import USER_ACTION_TYPES from '../../store/user/user.types';
-import ProductInterface from '../../interfaces/product-interface';
+// component imports
+import SnackBar from "../snackbar/snackbar.component";
+
+// state management imports
+import { selectUser } from "../../store/user/user.selector";
+
+// props or interfaces imports
+import OrderListItemProps, {
+  ProductListProps,
+  ProductItemProps,
+} from "./order-list-item.interface";
+import ProductInterface from "../../interfaces/product-interface";
+
+// styling imports
+import {
+  DateSC,
+  ContentSC,
+  ListItemSC,
+  MediaSC,
+  IDSC,
+  ProductItemSC,
+  ProductNameSC,
+  ProductPriceSC,
+} from "./order-list-item.styles";
 
 const getRatingText = (rating: number) => {
-  let label = '';
+  let label = "";
 
   switch (rating) {
     case 1:
-      label = 'Poor';
+      label = "Poor";
       break;
     case 2:
-      label = 'Fair';
+      label = "Fair";
       break;
     case 3:
-      label = 'Average';
+      label = "Average";
       break;
     case 4:
-      label = 'Good';
+      label = "Good";
       break;
     case 5:
-      label = 'Excellent';
+      label = "Excellent";
       break;
     default:
-      label = '';
+      label = "";
       break;
   }
 
-  return `Your rating: ${rating} (${label})`; 
-}
+  return `Your rating: ${rating} (${label})`;
+};
 
 const ProductList: FC<ProductListProps> = ({ productList }) => {
-  return (<>
-    {productList.map((productItem: ProductInterface) => {
-      return <ProductItem key={`productItem-${productItem._id}`} productItem={productItem} />
-    })}
-  </>);
-}
+  return (
+    <>
+      {productList.map((productItem: ProductInterface) => {
+        return (
+          <ProductItem
+            key={`productItem-${productItem._id}`}
+            productItem={productItem}
+          />
+        );
+      })}
+    </>
+  );
+};
 
 const ProductItem: FC<ProductItemProps> = ({ productItem }) => {
-  return (<>
-    <ProductItemSC>
-      <MediaSC image={productItem.imgUrls[0]} />
-      <ContentSC>
-        <ProductNameSC>{productItem.name}</ProductNameSC>
-        <ProductPriceSC>{`$${productItem.price}`}</ProductPriceSC>
-      </ContentSC>
-    </ProductItemSC>
-  </>);
-}
+  return (
+    <>
+      <ProductItemSC>
+        <MediaSC image={productItem.imgUrls[0]} />
+        <ContentSC>
+          <ProductNameSC>{productItem.name}</ProductNameSC>
+          <ProductPriceSC>{`$${productItem.price}`}</ProductPriceSC>
+        </ContentSC>
+      </ProductItemSC>
+    </>
+  );
+};
 
 const OrderListItem: FC<OrderListItemProps> = ({ order }) => {
   const navigate = useNavigate();
@@ -84,20 +110,19 @@ const OrderListItem: FC<OrderListItemProps> = ({ order }) => {
 
   return (
     <>
-    <ListItemSC>
-      <DateSC>{order.orderDate}</DateSC>
-      <IDSC>{`Order ID: #${order.orderID}`}</IDSC>
-      <ProductList productList={order.products} />
-    </ListItemSC>
-    <SnackBar
-      type={snackbar.type}
-      message={snackbar.message}
-      onClose={snackbarCloseHandler}
-      open={snackbar.open}
-    />
+      <ListItemSC>
+        <DateSC>{order.orderDate}</DateSC>
+        <IDSC>{`Order ID: #${order.orderID}`}</IDSC>
+        <ProductList productList={order.products} />
+      </ListItemSC>
+      <SnackBar
+        type={snackbar.type}
+        message={snackbar.message}
+        onClose={snackbarCloseHandler}
+        open={snackbar.open}
+      />
     </>
-    
-  )
-}
+  );
+};
 
 export default OrderListItem;
