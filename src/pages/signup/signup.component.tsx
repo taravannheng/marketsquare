3d0d989af8e19cf00a6de2669fb29c6df138d66a
Taravann Heng <heng.t.taravann@gmail.com>
2024-02-29 17:29:15 +0700

    refactor(signup): re-arrange import statements

--- src/pages/signup/signup.component.tsx
+++ src/pages/signup/signup.component.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react';
 
+// component imports
 import Header from '../../components/header/index.component';
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
+// styling imports
 import { SignUpPageSC, SignUpFormContainerSC } from './signup.styles';
 
 const SignUpPage: FC = () => {
