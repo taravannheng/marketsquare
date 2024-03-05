72283cccfa6e77bc9c0f7b571115b22e5f44dcf1
Taravann Heng <heng.t.taravann@gmail.com>
2024-03-05 21:57:46 +0700

    refactor(navigation-menu): rename component files

--- src/components/navigation-menu/navigation-menu.component.tsx
+++ src/components/navigation-menu/navigation-menu.component.tsx
@@ -7,11 +7,11 @@ import _ from 'lodash';
 import NavigationMenuItem from '../navigation-menu-item/index.component';
 
 // props or interfaces imports
-import NavigationMenuProps from './index.interface'
-import { MenuInterface } from './index.interface';
+import NavigationMenuProps from './navigation-menu.interface'
+import { MenuInterface } from './navigation-menu.interface';
 
 // styling imports
-import { NavigationMenuSC, NavigationMenuStackSC } from './index.styles';
+import { NavigationMenuSC, NavigationMenuStackSC } from './navigation-menu.styles';
 
 const NavigationMenu: FC<NavigationMenuProps> = ({ menuList }) => {
   return (
